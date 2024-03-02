38be90da434f3fa88f0fdf2a20d8ac0c3b652d2c
Sebastian Allard <seallards@gmail.com>
2024-03-02 18:28:09 +0100

    Add dimacs parser

---
+++ src/parser/parser.rs
@@ -0,0 +1,32 @@
+pub fn parse_dimacs(path: impl AsRef<Path>) -> io::Result<Formula> {
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut formula = Formula::new();
+
+    for line in reader.lines() {
+        let line = line?;
+
+        if is_comment_or_header(&line) {
+            continue;
+        }
+
+        let clause: Clause = parse_clause(&line);
+
+        if !clause.is_empty() {
+            formula.push(clause);
+        }
+    }
+    Ok(formula)
+}
+
+fn is_comment_or_header(line: &str) -> bool {
+    line.starts_with('c') || line.starts_with('p')
+}
+
+fn parse_clause(line: &str) -> Clause {
+    line.split_whitespace()
+        .filter_map(|lit| lit.parse::<Literal>().ok())
+        .filter(|&lit| lit != 0)
+        .collect()
+}
