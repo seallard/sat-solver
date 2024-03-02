1d80b733cc37885259ad8986ce85c62e093f1cb1
Sebastian Allard <seallards@gmail.com>
2024-03-02 22:33:34 +0100

    Clean up

--- tests/test_parser.rs
+++ tests/test_parser.rs
@@ -1,24 +1,15 @@
-use rstest::{fixture, rstest};
-use std::fs::File;
-use std::io::Write;
-use std::path::PathBuf;
-use tempfile::tempdir;
-
 use sat_solver::parser::parser::parse_dimacs;
+use sat_solver::models::formula::Formula;
 
-#[fixture]
-pub fn dimacs_file() -> PathBuf {
-    let dir = tempdir().unwrap();
-    let file_path = dir.path().join("test.dimacs");
-    let mut file = File::create(&file_path).unwrap();
-    writeln!(file, "c This is a comment\np cnf 3 2\n1 -3 0\n2 3 -1 0").unwrap();
-    file_path
-}
 
-#[rstest]
-fn test_parse_dimacs(dimacs_file: PathBuf) {
-    let formula = parse_dimacs(dimacs_file).unwrap();
+#[test]
+fn test_parse_dimacs() {
+    // GIVEN a valid DIMACS file
+    let file = "tests/test.dimacs";
 
+    // WHEN the file is parsed
+    let formula: Formula = parse_dimacs(file).unwrap();
+
+    // THEN the formula is returned
     assert_eq!(formula.len(), 2);
-    // Further assertions as needed
 }
